09f5b972cef8dc249d3fa41bcaf121833baffb1d
Edenn Touitou <deozza@gmail.com>
2023-05-11 16:02:46 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    import TanoshiHeaderModel from 'tanoshi/src/lib/atoms/header/TanoshiHeaderModel';
+    import TanoshiHeader from 'tanoshi/src/lib/atoms/header/TanoshiHeader.svelte';
+
+    const testHeader: TanoshiHeaderModel = new TanoshiHeaderModel('Welcome to Sveltekit').setDisplaySize('6xl').setHtmlSize('h1')
+
+</script>
+
+<TanoshiHeader tanoshiHeaderModel={testHeader}/>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
